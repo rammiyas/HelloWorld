9b1efd7b808d1b5fb7d8b34f3ba179e24d8eacf7
Rammiya Siva <rammiya@live.co.uk>
2018-04-11 17:01:25 +0100

    Fix bug; improve debug output

--- program.go
+++ program.go
@@ -1,5 +1,7 @@
 package main
 
+import "strconv"
+
 func main() {
 	/*If we list all the natural numbers below 10 that are multiples of 3 and 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
 	Find the sum of all the multiples of 3 and 5 below 1000.
@@ -9,10 +11,9 @@ func main() {
 	var i = 0
 	var total = 0
 	for i = 1; i < 1000; i++{
-		if i % 3 == 0 || i % 5 == 0 {
+		if i % 3 == 0 && i % 5 == 0 {
 			total = total + i
-
 		}
 	}
-	println(total)
+	println("Sum = " + strconv.Itoa(total))
 }
